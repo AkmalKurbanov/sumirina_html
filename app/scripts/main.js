1e7462bda27d202f2449d79e48fb624df9a7b9a4
AkmalKurbanov <250296a@gmail.com>
2019-02-22 04:15:34 +0600

    header section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,53 @@
+$(document).ready(function () {
 
+
+
+  // hamburger
+  $('.hamburger').click(function () {
+    $(this).toggleClass('openClose');
+    $('.mnu-js').toggleClass('mobileMnu-js');
+    $('.hamburger__line').toggleClass('lineBg');
+  });
+
+  $('.mnu-js').click(function () {
+    $(this).removeClass('mobileMnu-js');
+    $('.hamburger').removeClass('openClose');
+    $('.hamburger__line').removeClass('lineBg');
+  });
+  // hamburger end
+
+
+
+  // scroll button
+  function scrollToSection(event) {
+    event.preventDefault();
+    var $section = $($(this).attr('href'));
+    $('html, body').animate({
+      scrollTop: $section.offset().top
+    }, 700);
+  }
+  $('[data-scroll]').on('click', scrollToSection);
+  // scroll button end
+
+
+
+
+
+  // count
+  $('.count-js').each(function () {
+    $(this).prop('Counter', 0).animate({
+      Counter: $(this).text()
+    }, {
+      duration: 4000,
+      easing: 'swing',
+      step: function (now) {
+        $(this).text(Math.ceil(now));
+      }
+    });
+  });
+  // count end
+
+
+
+
+});
